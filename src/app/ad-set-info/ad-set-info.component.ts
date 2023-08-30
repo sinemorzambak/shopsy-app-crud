b4f38891b55a64fc6fe69c288ec2558de36f7090
sinemorzambak <sinemorzambak@gmail.com>
2023-08-31 02:19:51 +0300

    feat(ad-set-info): add css designs and image props

--- src/app/ad-set-info/ad-set-info.component.ts
+++ src/app/ad-set-info/ad-set-info.component.ts
@@ -3,62 +3,50 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../store/app.state';
 import { setCampaignInfo } from '../store/campaign.actions';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
-
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 interface Product {
   'product-name': string;
   'product-price': number;
   'product-stock': string;
-  'product-image-url': string; 
+  'product-image-url': string;
 }
 
 @Component({
   selector: 'app-ad-set-info',
   templateUrl: './ad-set-info.component.html',
-  styleUrls: ['./ad-set-info.component.css']
+  styleUrls: ['./ad-set-info.component.css'],
 })
 export class AdSetInfoComponent implements OnInit {
   adGroupName: string = '';
   availableProducts: Product[] = [];
   selectedProducts: Product[] = [];
 
-  constructor(
-    private store: Store<AppState>,
-    private firestore: AngularFirestore,
-    private storage: AngularFireStorage
-  ) {}
+  constructor(private store: Store<AppState>, private firestore: AngularFirestore) {}
 
   ngOnInit() {
-    this.firestore.collection<Product>('products').valueChanges().subscribe((products: Product[]) => {
-      this.availableProducts = products;
-    });
+    this.firestore
+      .collection<Product>('products')
+      .valueChanges()
+      .subscribe((products: Product[]) => {
+        this.availableProducts = products.map((product) => {
+          return {
+            ...product,
+            'product-image-url': 'assets/images.jpg', 
+          };
+        });
+      });
 
-
-    const storageInstance = getStorage();
-    const productImageRef = ref(storageInstance, 'tshirt.jpg'); 
-
-    
-    getDownloadURL(productImageRef).then((url) => {
-      const productWithImage: Product = {
-        'product-name': 'Product with Image',
-        'product-price': 20,
-        'product-stock': 'In Stock',
-        'product-image-url': url, 
-      };
-      this.availableProducts.push(productWithImage);
-    });
-
-    this.store.select(state => state.campaignState.campaignInfo).subscribe(campaignInfo => {
-      this.selectedProducts = campaignInfo.selectedProducts || [];
-    });
+    this.store
+      .select((state) => state.campaignState.campaignInfo)
+      .subscribe((campaignInfo) => {
+        this.selectedProducts = campaignInfo.selectedProducts || [];
+      });
   }
 
   continueNextStep() {
     const updatedCampaignInfo = {
       adGroupName: this.adGroupName,
-      selectedProducts: this.selectedProducts
+      selectedProducts: this.selectedProducts,
     };
     this.store.dispatch(setCampaignInfo({ campaignInfo: updatedCampaignInfo }));
   }
