8f9e5f2032760de0fbbcf66fe2337ddf199e2581
sinemorzambak <sinemorzambak@gmail.com>
2023-09-04 15:52:34 +0300

    feat(campaign-list): add status column

--- src/app/campaign-list/campaign-list.component.ts
+++ src/app/campaign-list/campaign-list.component.ts
@@ -12,21 +12,25 @@ export class CampaignListComponent implements OnInit {
 
   ngOnInit() {
     const storedCampaignState = localStorage.getItem('campaignState');
-
+  
     if (storedCampaignState) {
       const campaignState = JSON.parse(storedCampaignState);
       
       const campaignInfo = campaignState.campaign.campaignState.campaignInfo;
       const createdCampaign = campaignState.campaign.campaignState.createdCampaign;
-
+  
       if (campaignInfo && createdCampaign) {
+        const status = createdCampaign ? 'Running' : 'Not Started';
+  
         this.campaigns.push({
           campaignName: createdCampaign.campaignName,
           startDate: createdCampaign.startDate,
           endDate: createdCampaign.endDate,
           adGroupName: campaignInfo.adGroupName,
+          status: status, 
         });
       }
     }
   }
+  
 }
